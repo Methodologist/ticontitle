016e335d2e1c3c3439043c481a55881a21605c15
Methodologist <christophergillig@gmail.com>
2017-02-10 11:19:59 -0500

    splash screen should close by default

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+import {Splashscreen} from 'ionic-native';
+...
+    hideSplashScreen() {
+        if (Splashscreen) {
+            setTimeout(() => {
+                Splashscreen.hide();
+            }, 100);
+        }
+    })
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
